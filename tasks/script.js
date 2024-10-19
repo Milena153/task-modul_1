8fd9b45e462a7585bb4c69e7c9c124729a4a3209
Milena153 <DukeSachenko14@gmail.com>
2024-10-19 17:25:27 +0300

    feat: add 1-3 task

---
+++ tasks/script.js
@@ -0,0 +1,30 @@
+//задание 1
+
+//let num = prompt('Введите любое число,чтобы проверить, является ли оно четным или нечетным:');
+//  if (num % 2 == 0 ){
+//     console.log('Число четное')
+//  }else{
+//     console.log('Предоставленное число нечетное') 
+//  };
+
+//задание 2
+
+// let num1 = prompt('Введите первое число:');
+// let num2 = prompt('Введите второе число:');
+//  if (num1 == num2 ){
+//     console.log('Числа равны');
+//  }else if(num1 > num2){
+//     console.log('первое число больше');
+//  }else if(num1 < num2){
+//     console.log('второе число больше');
+//  }
+
+// задание 3
+
+// let nameLogin = prompt('Введите логин: ');
+// let password = prompt('Введите пароль:');
+//  if (nameLogin == 'user123' && password == '321' ){
+//     console.log('Доступ разрешен');
+//  }else{
+//     console.log('Доступ запрещен');
+//  }
