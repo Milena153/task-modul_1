9d2ffd05329f58ff6d8f6accd39482d2d7ceb987
Milena153 <DukeSachenko14@gmail.com>
2024-10-24 21:52:45 +0300

    init:generelization

--- tasks/script.js
+++ tasks/script.js
@@ -28,3 +28,98 @@
 //  }else{
 //     console.log('Доступ запрещен');
 //  }
+
+//задание 4
+
+// let n = prompt('введите число n: ');
+// let numb = 1;
+// let sum = 0;
+// while (numb < n) {
+//     sum += numb;
+//     numb++;
+//   }
+// console.log(sum);
+
+//задание 5
+
+// for (let i = 1; i <= 3;  i += 1) {
+//     for (let j = 1; j <= 9; j += 1) {
+//       console.log(`${i} x ${j} = ${i * j}`);
+//     }
+//   };
+
+//задание 6
+
+// let num1 = prompt('введите первое число: ');
+// let num2 = prompt('введите второе число: ');
+// let num3 = prompt('введите третье число: ');
+// let maxNum = Math.max(num1,num2,num3);
+// alert(maxNum)
+
+//задание 7
+
+// for (let i = 1; i <=20 ; i++) { // выведет 0, затем 1, затем 2
+//     if (i % 3 == 0){
+//         console.log(i);
+//     }
+//   }
+
+//задание 8
+//Напишите программу, которая запрашивает у пользователя число и проверяет, является ли оно положительным. НЕ ПОНЯЛА КАК ЭТО ДЕЛАТЬ 
+
+//задание 9
+
+// let a = prompt('введите номер 1-7: ') ;
+// switch (a) {
+//     case '1':
+//     alert('Понедельник');
+//     case '2':
+//     alert('Вторник');
+//     break;
+//     case '3':
+//     alert('Среда');
+//     break;
+//     case '4':
+//     alert('Четверг');
+//     break;
+//     case '5':
+//     alert('Пятница');
+//     break;
+//     case '6':
+//     alert('Суббота');
+//     break;
+//     case '7':
+//     alert('Воскресенье');
+//     break;
+//     default:
+//     alert('Нет таких значений');
+// }
+
+//задание 9
+
+// function greet(name) {
+//   console.log(`hello,${name}!`);
+// }
+// greet('milena')
+
+//задание 10
+
+// function sum(a,b){
+//     return  a + b;
+    
+// }
+
+//задание 11
+
+// function circleArea(r){
+//     return Math.PI * r * r;
+// }
+
+//задание 12
+
+// function isEven(n){
+//     return n % 2 === 0;
+// }
+
+//Задание 13
+//Функция преобразования температуры, Напишите функцию convertTemperature(c). НЕ ПОНЯЛА КАК ЭТО ДЕЛАТЬ
